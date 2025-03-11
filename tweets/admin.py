635af23553d3a2b0ecce8ac92e7ce9b098d9eed2
Nova <bushdog094@gmail.com>
2025-03-11 18:05:58 +0900

    admin filter, search!

--- tweets/admin.py
+++ tweets/admin.py
@@ -3,6 +3,24 @@ from .models import Tweet
 from .models import Like
 
 
+class ElonMuskFilter(admin.SimpleListFilter):
+    title = "Elon_Filter"
+    parameter_name = "elon_musk"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("contains", "Contains 'Elon Musk'"),
+            ("not_contains", "Doesn't contain 'Elon Musk'"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "contains":
+            return queryset.filter(payload__icontains="Elon Musk")
+        if self.value() == "not_contains":
+            return queryset.exclude(payload__icontains="Elon Musk")
+        return queryset
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
 
@@ -13,13 +31,11 @@ class TweetAdmin(admin.ModelAdmin):
         "updated_at",
         "like_count",
     )
-    list_filter = ("user", "created_at")
-    search_fields = ("user",)
+    list_filter = ("user__username", "created_at", ElonMuskFilter)
+    search_fields = ("user__username",)
 
     def short_payload(self, obj):
         return f"{obj.payload[:4]}..."
-
-    short_payload.short_description = "Payload"
 
 
 @admin.register(Like)
@@ -31,5 +47,5 @@ class LikeAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
-    list_filter = ("user", "tweet")
-    search_fields = ("user",)
+    list_filter = ("user__username", "created_at", ElonMuskFilter)
+    search_fields = ("user__username", "tweet__payload")
--- users/models.py
+++ users/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):  # Django 기본 User 모델 확장
+class User(AbstractUser):
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
     name = models.CharField(max_length=150, default="")
 
     def __str__(self):
-        return f"@{self.username}"  # "트위터 스타일로 표시"
+        return f"@{self.username}"

10336ec777a947c4b757c029727f3df46a5abac0
Nova <bushdog094@gmail.com>
2025-03-13 03:03:11 +0900

    render()

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Tweet  # Tweet 모델 가져오기
 
-# Create your views here.
+
+def tweet_list(request):
+    tweets = Tweet.objects.all()  # ORM을 사용하여 모든 Tweet 가져오기
+    return render(
+        request,
+        "tweet_list.html",
+        {
+            "tweets": tweets,
+            "title": "Twit List",
+        },
+    )  # 템플릿에 데이터 전달
